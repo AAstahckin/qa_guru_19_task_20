0961b3dcc038a38c50d242e8a0578ee747e6582a
AAstahckin <ALVAstashkin91@yandex.ru>
2023-06-21 23:16:13 +0300

    add fix assertionsAddress

--- src/test/java/responseassertions/AssertAddressResponse.java
+++ src/test/java/responseassertions/AssertAddressResponse.java
@@ -13,22 +13,29 @@ public class AssertAddressResponse {
     public static void assertionsAddressResponse(String page, Map<String, String> data) {
         Document document = Jsoup.parse(page);
         step("Проверяем что параметр phone присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("phone").text(), "Phone number: " + data.get("Address.PhoneNumber")));
+                assertEquals(document.getElementsByClass("phone").text(),
+                        "Phone number: " + data.get("Address.PhoneNumber")));
         step("Проверяем что параметр title присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("title").text(), "My account " + data.get("Address.FirstName") + " " + data.get("Address.LastName")));
+                assertEquals(document.getElementsByClass("title").text(),
+                        "My account " + data.get("Address.FirstName") + " " + data.get("Address.LastName")));
         step("Проверяем что параметр name присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("name").text(),
-                        "Build your own cheap computer " + data.get("Address.FirstName") + " " + data.get("Address.LastName")));
+                assertEquals(document.select("li.name").first().text(),
+                        data.get("Address.FirstName") + " " + data.get("Address.LastName")));
         step("Проверяем что параметр email присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("email").text(), "Email: " + data.get("Address.Email")));
+                assertEquals(document.getElementsByClass("email").text(),
+                        "Email: " + data.get("Address.Email")));
         step("Проверяем что параметр fax присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("fax").text(), "Fax number: " + data.get("Address.FaxNumber")));
+                assertEquals(document.getElementsByClass("fax").text(),
+                        "Fax number: " + data.get("Address.FaxNumber")));
         step("Проверяем что параметр company присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("company").text(), data.get("Address.Company")));
+                assertEquals(document.getElementsByClass("company").text(),
+                        data.get("Address.Company")));
         step("Проверяем что параметр address1 присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("address1").text(), data.get("Address.Address1")));
+                assertEquals(document.getElementsByClass("address1").text(),
+                        data.get("Address.Address1")));
         step("Проверяем что параметр address2 присутствует в ответе", () ->
-                assertEquals(document.getElementsByClass("address2").text(), data.get("Address.Address2")));
+                assertEquals(document.getElementsByClass("address2").text(),
+                        data.get("Address.Address2")));
     }
 
 }
